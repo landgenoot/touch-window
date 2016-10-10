8774e4faff3740eaf0a78aa75f5e1ce5624e6cd6
Daan Middendorp <github-d@landgenoot.com>
2016-10-10 14:49:45 +0200

    Created theme and canvas

--- client/app.js
+++ client/app.js
@@ -4,10 +4,23 @@
 angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
-  'myApp.version'
+  'myApp.version',
+  'ngMaterial'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+}])
+.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('red')
+    .accentPalette('deep-orange');
+
+
+  $mdThemingProvider.enableBrowserColor({
+    theme: 'default', // Default is 'default'
+    palette: 'primary', // Default is 'primary', any basic material palette and extended palettes are available
+    hue: '1000' // Default is '800'
+  });
+});
